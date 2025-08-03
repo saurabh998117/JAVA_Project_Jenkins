42a8c451fc7308127a84b07685a1a26bea280e29
saurabh998117 <139375158+saurabh998117@users.noreply.github.com>
2025-08-03 13:10:23 +0530

    update code

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public Student(){
+        System.out.println("creating student object");
+         
+
+        }
+        public void sayhello(){
+            System.out.println("hello! i am student");
+            System.out.println("thankyou");
+    }
+    
+}
